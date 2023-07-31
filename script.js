4f1fb08ff00b4183e6698b6ed658be4e50f98902
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-31 18:06:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function malachi() {
     if (prompt("WHATS THE PASSWORD") == "malachil0612") {
         window.location.href = "malachi/index.html"
     }
+    else {
+        alert('the password you entered is incorrect')
+    }
 };
 function ezekiel() {
     if (prompt("WHATS THE PASSWORD") == "") {
         window.location.href = "ezekiel/index.html"
+    }
+    else {
+        alert('the password you entered is incorrect')
     }
 };
