eb5ef50272a766007c0c49a9891c0b48c45e722d
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-31 17:58:59 -0500

    added password for malachi

---
+++ script.js
@@ -0,0 +1,10 @@
+function malachi() {
+    if (prompt("WHATS THE PASSWORD") == "malachil0612") {
+        window.location.href = "malachi/index.html"
+    }
+};
+function ezekiel() {
+    if (prompt("WHATS THE PASSWORD") == "") {
+        window.location.href = "ezekiel/index.html"
+    }
+};
